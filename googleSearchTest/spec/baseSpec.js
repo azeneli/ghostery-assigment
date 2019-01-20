5441d1ad724c9a11be73ded745f4de4ee02a5703
Anri Zeneli <anri@seccosquared.com>
2019-01-20 01:23:36 -0500

    added test suite for enter key

--- googleSearchTest/spec/baseSpec.js
+++ googleSearchTest/spec/baseSpec.js
@@ -1,13 +1,36 @@
-describe('testing google ping', () => {
+// describe('Ensure that you are in the proper site', () => {
 
-	it('should ping to google', () =>{
-		browser.get('https://www.google.com/');
-		const googleUrl = 'https://www.google.com/';
+// 	it('should ping to google', () =>{
+// 		browser.get('https://www.google.com/');
+// 		const googleUrl = 'https://www.google.com/';
 
-		expect(browser.getCurrentUrl()).toBe(googleUrl);	
+// 		expect(browser.getCurrentUrl()).toBe(googleUrl);	
+// 	});
+
+// });
+
+//import basePage from '../pages/basePage';
+
+describe('Search Functionality', () => {
+	beforeEach(() =>  {
+		browser.get('https://www.google.com/');	
+	});
+
+	it('should search the word by hitting key', () =>{
+		const inputForm = $$('input.gLFyf.gsfi');		
+		let searchQuery = 'ghostery';
+	
+		inputForm.sendKeys(searchQuery);
+		browser.actions().sendKeys(protractor.Key.ENTER).perform();
+
+		// testing for page title as it indicates the you reached results page
+		expect(browser.getTitle()).toBe('ghostery - Google Search');
 	});
 
 
-
-
 });
+
+
+
+
+// search results page class h1.bNg8Rb
